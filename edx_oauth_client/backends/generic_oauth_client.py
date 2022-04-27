943b90d0f293126c5ae0a7149815a9d01febc4a8
Hein <heinlinaung6891@gmail.com>
2022-04-27 13:36:27 +0630

    disabled state param

--- edx_oauth_client/backends/generic_oauth_client.py
+++ edx_oauth_client/backends/generic_oauth_client.py
@@ -1,57 +1,39 @@
-import logging
-import urlparse
+"""
+ProxteraOAuth2: Proxtera OAuth2
+"""
 
-from django.conf import settings
-from social.backends.oauth import BaseOAuth2
-from social.utils import handle_http_errors
+import urllib
+from social_core.backends.oauth import BaseOAuth2
+from social_core.exceptions import AuthFailed
+from social_core.utils import handle_http_errors
+from common.djangoapps import third_party_auth
 
-import third_party_auth
-
-log = logging.getLogger(__name__)
-
-DEFAULT_AUTH_PIPELINE = [
-    'third_party_auth.pipeline.parse_query_params',
-    'social.pipeline.social_auth.social_details',
-    'social.pipeline.social_auth.social_uid',
-    'social.pipeline.social_auth.auth_allowed',
-    'social.pipeline.social_auth.social_user',
-    'third_party_auth.pipeline.associate_by_email_if_login_api',
-    'social.pipeline.user.get_username',
-    'third_party_auth.pipeline.set_pipeline_timeout',
-    'edx_oauth_client.pipeline.ensure_user_information',
-    'social.pipeline.user.create_user',
-    'social.pipeline.social_auth.associate_user',
-    'social.pipeline.social_auth.load_extra_data',
-    'social.pipeline.user.user_details',
-    'third_party_auth.pipeline.set_logged_in_cookies',
-    'third_party_auth.pipeline.login_analytics'
-]
-
-
-class GenericOAuthBackend(BaseOAuth2):
+class ProxteraOAuth2(BaseOAuth2):
     """
-    Backend for Generic OAuth Server Authorization.
+    Backend for Proxtera OAuth Server Authorization.
     """
-    name = 'custom-oauth2'
+    name = 'proxtera-oauth2'
+    SUCCEED = True  # You can patch this during tests in order to control whether or not login works
 
-    CUSTOM_OAUTH_PARAMS = settings.CUSTOM_OAUTH_PARAMS
+    # CUSTOM_OAUTH_PARAMS = settings.CUSTOM_OAUTH_PARAMS
 
-    if not all(CUSTOM_OAUTH_PARAMS.values()):
-        log.error("Some of the CUSTOM_OAUTH_PARAMS are improperly configured. Custom oauth won't work correctly.")
+    # if not all(CUSTOM_OAUTH_PARAMS.values()):
+        # log.error("Some of the CUSTOM_OAUTH_PARAMS are improperly configured. Custom oauth won't work correctly.")
 
-    PROVIDER_URL = CUSTOM_OAUTH_PARAMS.get('PROVIDER_URL')
-    AUTHORIZE_URL = CUSTOM_OAUTH_PARAMS.get('AUTHORIZE_URL')  # '/oauth2/authorize' usually is default value
-    GET_TOKEN_URL = CUSTOM_OAUTH_PARAMS.get('GET_TOKEN_URL')  # '/oauth2/token' usually is default value
-    ID_KEY = CUSTOM_OAUTH_PARAMS.get('PROVIDER_ID_KEY')  # unique marker which could be taken from the SSO response
-    USER_DATA_URL = CUSTOM_OAUTH_PARAMS.get('USER_DATA_URL')  # '/api/current-user/' some url similar to the example
+    PROVIDER_URL = "https://devauth.proxtera.app"
+    AUTHORIZE_URL = "/oauth2/authorize"  # '/oauth2/authorize' usually is default value
+    GET_TOKEN_URL = "/oauth2/token"  # '/oauth2/token' usually is default value
+    ID_KEY = "username"  # unique marker which could be taken from the SSO response
+    USER_DATA_URL = "https://devauthapi.proxtera.app/oauth2/userinfo"  # '/api/current-user/' some url similar to the example
 
-    AUTHORIZATION_URL = urlparse.urljoin(PROVIDER_URL, AUTHORIZE_URL)
-    ACCESS_TOKEN_URL = urlparse.urljoin(PROVIDER_URL, GET_TOKEN_URL)
-    DEFAULT_SCOPE = settings.FEATURES.get('SCOPE')  # extend the scope of the provided permissions.
+    AUTHORIZATION_URL = urllib.parse.urljoin(PROVIDER_URL, AUTHORIZE_URL)
+    ACCESS_TOKEN_URL = urllib.parse.urljoin(PROVIDER_URL, GET_TOKEN_URL)
+    # DEFAULT_SCOPE = settings.FEATURES.get('SCOPE')  # extend the scope of the provided permissions.
+    DEFAULT_SCOPE = ['email']
     REDIRECT_STATE = False
+    STATE_PARAMETER = False
+    RESPONSE_TYPE = 'code'
     ACCESS_TOKEN_METHOD = 'POST'  # default method is 'GET'
-
-    PIPELINE = DEFAULT_AUTH_PIPELINE
 
     skip_email_verification = True
 
@@ -70,13 +52,48 @@ class GenericOAuthBackend(BaseOAuth2):
                 return provider_config.get_setting(name)
             except KeyError:
                 pass
-        return super(GenericOAuthBackend, self).setting(name, default=default)
+        return super(ProxteraOAuth2, self).setting(name, default=default)
+
+    def auth_params(self, state=None):
+        client_id, client_secret = self.get_key_and_secret()
+        params = {
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'redirect_uri': self.get_redirect_uri(state)[:-1],
+            'scope': 'email',
+            'response_type': 'code'
+        }
+        return params
+
+    # def auth_complete_params(self, state=None):
+    #     client_id, client_secret = self.get_key_and_secret()
+    #     # # Sample Request
+    #     # {
+    #     #     "clientId": "9ce7a185f5c84dd5",
+    #     #     "clientSecret": "42fb7717573f473e96af53367bae0d66aab",
+    #     #     "redirectUri": "http://localhost:4009",
+    #     #     "code": "xxx",
+    #     #     "grantType": "authorization_code"
+    #     # }
+    #     return {
+    #         'grantType': 'authorization_code',  # request auth code
+    #         'code': self.data.get('authorizationCode', ''),  # server response code
+    #         'clientId': client_id,
+    #         'clientSecret': client_secret,
+    #         'redirectUri': self.get_redirect_uri(state)[:-1]
+    #     }
 
     def get_user_details(self, response):
         """
         Return user details from SSO account.
         """
-        return response
+        data = response.get('data')
+        return {'username': data.get('username'),
+                'name': data.get('fullname'),
+                'fullname': data.get('fullname'),  
+                'email': data.get('email') or '',
+                'first_name': data.get('firstname'),
+                'last_name': data.get('lastname')}
 
     @handle_http_errors
     def do_auth(self, access_token, *args, **kwargs):
@@ -97,24 +114,24 @@ class GenericOAuthBackend(BaseOAuth2):
         self.strategy.session_set('{}_state'.format(self.name), self.data.get('state'))
         next_url = '/'
         self.strategy.session.setdefault('next', next_url)
-        return super(GenericOAuthBackend, self).auth_complete(*args, **kwargs)
+        return super(ProxteraOAuth2, self).auth_complete(*args, **kwargs)
 
     def user_data(self, access_token, *args, **kwargs):
         """
         Grab user profile information from SSO.
         """
         data = self.get_json(
-            urlparse.urljoin(self.PROVIDER_URL, self.USER_DATA_URL),
+            self.USER_DATA_URL,
             params={'access_token': access_token},
         )
         data['access_token'] = access_token
         return data
 
-    def pipeline(self, pipeline, pipeline_index=0, *args, **kwargs):
-        self.strategy.session.setdefault('auth_entry', 'register')
-        return super(GenericOAuthBackend, self).pipeline(
-            pipeline=self.PIPELINE, *args, **kwargs
-        )
+    # def pipeline(self, pipeline, pipeline_index=0, *args, **kwargs):
+    #     self.strategy.session.setdefault('auth_entry', 'register')
+    #     return super(ProxteraOAuth2, self).pipeline(
+    #         pipeline=self.PIPELINE, *args, **kwargs
+    #     )
 
     def get_user_id(self, details, response):
         """
